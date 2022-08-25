2cb186ebdd9d42c0947bf3e5dd09fa99a50eb34f
Juliya Khanenko <99866900+jullietIT@users.noreply.github.com>
2022-08-25 15:57:31 +0300

    src

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,47 +1,50 @@
-// При загрузке страницы проверяй состояние хранилища, и если там есть сохраненные данные, заполняй ими поля формы. В противном случае поля должны быть пустыми.
-
-
-// 4.Сделай так, чтобы хранилище обновлялось не чаще чем раз в 500 миллисекунд. Для этого добавь в проект и используй библиотеку lodash.throttle.
 import throttle from 'lodash.throttle';
 
-
-
 const form = document.querySelector('.feedback-form');
-const LOCALSTORAGE_KEY = 'feedback-form-state';
-form.addEventListener('input', throttle(onFormData, 500));
-form.addEventListener('submit', onSubmitForm);
-
-const formData = {};
+const email = document.querySelector('[name=email]');
+const text = document.querySelector('[name=message]');
 
 
+const TEXT_INPUT = 'feedback-form-state';
+const formInput = {};
 
-function onFormData(e) {
-  formData[e.target.name] = e.target.value;
-  localStorage.setItem('LOCALSTORAGE_KEY', JSON.stringify(formData));
+
+form.addEventListener('input', throttle(onInput, 500));
+
+form.addEventListener('submit', onSubmit);
+showSavedInput();
+
+function onInput(e) {
+
+  formInput[e.target.name] = e.target.value;
+
+  const saveText = JSON.stringify(formInput);
+ 
+  localStorage.setItem(TEXT_INPUT, saveText);
 }
 
-
-//3. При сабмите формы очищай хранилище и поля формы, а также выводи объект с полями email, message и текущими их значениями в консоль.
-function onSubmitForm(e) {
-  console.log(JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY')));
+function onSubmit(e) {
+  const save = localStorage.getItem(TEXT_INPUT);
   e.preventDefault();
-  e.currentTarget.reset();
-  localStorage.removeItem('LOCALSTORAGE_KEY');
+  console.log(JSON.parse(save));
+  localStorage.removeItem(TEXT_INPUT);
+  e.target.reset();
 }
 
-// 1.Отслеживай на форме событие input, и каждый раз записывай в локальное хранилище объект с полями email и message, в которых сохраняй текущие значения полей формы. Пусть ключом для хранилища будет строка "feedback-form-state".
+function showSavedInput() {
+  const save = localStorage.getItem(TEXT_INPUT);
 
-(function dataFromLocalStorage() {
-  const dataStorage = JSON.parse(localStorage.getItem('LOCALSTORAGE_KEY'));
+  if (save) {
+    const textInsert = JSON.parse(save);
+    console.log(textInsert);
+    textInsert.message
+      ? (text.value = textInsert.message)
+      : (text.value = '');
 
-  const email = document.querySelector('.feedback-form input');
-  const message = document.querySelector('.feedback-form textarea');
+    textInsert.email
+      ? (email.value = textInsert.email)
+      : (email.value = '');
 
-  if (dataStorage) {
-    email.value = dataStorage.email;
-    message.value = dataStorage.message;
+
   }
-})();
-
-
-
+}
